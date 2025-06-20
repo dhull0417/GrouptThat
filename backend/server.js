52bce78ae58e3ccd7d082bbdcc3452a91c400bea
Dallin Hull <dallin.hull@outlook.com>
2025-06-19 21:04:19 -0600

    Test app.get and res.send

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,15 @@
 import express from "express";
+import { response } from "express";
 
 const app = express();
+
+app.get("/", (req, res) => {
+ res.send("Hello from the backend")
+})
+
+app.get("/test", (req, res) => {
+ res.send("Hello from Testing Testerton")
+})
 
 // Check package.json scripts
 app.listen(3000, () => {
