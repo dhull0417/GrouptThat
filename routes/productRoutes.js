24e05ac4e678095df2b869a35e2c09c2c59e26c8
Dallin Hull <dallin.hull@outlook.com>
2025-06-28 04:29:01 -0600

    ROUTER/CONTROLLER BASICS - product routes expressed in routes/productRoutes.js but functions (getAllProducts; createProducts) moved to controllers/productController.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,9 +1,11 @@
 import expres from "express";
+import { getAllProducts } from "../controllers/productController.js";
+import { createProduct } from "../controllers/productController.js";
 
 const router = expres.Router();
 
-router.get("/test", (req, res) => {
-    res.send("test route");
-})
+router.get("/test", getAllProducts)
+
+router.post("/test", createProduct)
 
 export default router;
